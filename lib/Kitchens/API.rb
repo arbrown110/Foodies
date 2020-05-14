cc071e268ca7b3f21d5130394c475ff5e4548cac
chic-caller-7520 <arbrown110@gmail.com>
2020-05-14 04:29:34 +0000

    working on api errors.

--- lib/Kitchens/API.rb
+++ lib/Kitchens/API.rb
@@ -3,7 +3,7 @@ class API
   def self.get_list(cuisine)
     
     key = ENV.fetch('MYSPOON-API-KEY')
-    url = "https://api.spoonacular.com/recipes/#{cuisine}/search?query=number?apiKey=#{ENV.fetch('MYSPOON-API-KEY')}&=10"
+    url = "https://api.spoonacular.com/recipes/#{cuisine}/search?=#{key}&query=number&=10"
     
     response = Net::HTTP.get(URI(url))
     dishes= JSON.parse(response)["results"]
@@ -19,7 +19,7 @@ class API
   
   def self.getdishsum(dish)
    key = ENV.fetch('MYSPOON-API-KEY')
-   url = "https://api.spoonacular.com/recipes/#{dish.dish_id}/#{summary}?apiKey=#{ENV.fetch('MYSPOON-API-KEY')}"
+   url = "https://api.spoonacular.com/recipes/#{dish.dish_id}/#{summary}=#{key}"
    response = Net::HTTP.get(URI(url))
    dishmen = JSON.parse(response).first
    dish.summary = dishmen["summary"]
@@ -27,12 +27,26 @@ class API
  
   def self.similar(dish)
    key = ENV.fetch ('MYSPOON-API-KEY')
-   url ="https://api.spoonacular.com/recipes/#{dish_id}/similar?apiKey=#{ENV.fetch('MYSPOON-API-KEY')}&number=2"
+   url ="https://api.spoonacular.com/recipes/#{dish_id}/similar?=#{key}&number=2"
    response = Net::HTTP.get(URI(url))
    suggestion= JSON.parse(response)
    dish.dish_id = suggestion["title"]
   end
-
+  
+  #def self.hungry(cuisine)
+ # key = ENV.fetch('MYSPOON-API-KEY')
+ # url = "https://api.spoonacular.com/recipes/complexSearch?=#{key}&cuisine=#{cuisine}&addRecipeInformation=true&number=10"
+ # response = Net::HTTP.get(URI(url))
+  # dishes= JSON.parse(response)["results"]
+  #  new_cuisine = Cuisine.new(cuisine)
+  #  dishes.each do |dish_menu|
+  #    name = dish_menu["title"]
+  #    drink_id = dish_menu["id"]
+  #    cuisine = dish_type ["cuisines"]
+      
+   #  new_dish = Dish.new(name: name, dish_id: drink_id, cuisine: cuisine)
+  #  new_cuisine.dishes << new_dish
+  
 end
     
   
